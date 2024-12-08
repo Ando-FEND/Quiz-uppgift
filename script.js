35e6c62a7cea865e3bf131d44a071f13179d696c
Ando-FEND <antonio.brynildsen@hotmail.com>
2024-12-08 18:06:05 +0100

    Ändrat i koden med språk

--- script.js
+++ script.js
@@ -1,98 +1,78 @@
-const question = [
+const questions = [
     { 
         question: "Vilken stad är bäst i Sverige",
         answers: [
-            {text: "Malmö", correct: false},
-            {text: "Göteborg", correct: false},
-            {text: "Stockholm", correct: true},
-            {text: "Borås", correct: false},
+            {text: "ABC", correct: true},
+            {text: "DEF", correct: false},
         ]
     },
     { 
         question: "Vilken stad är bäst i Sverige",
         answers: [
-            {text: "Malmö", correct: false},
-            {text: "Göteborg", correct: false},
-            {text: "Stockholm", correct: true},
-            {text: "Borås", correct: false},
+            {text: "Sant", correct: false},
+            {text: "Falskt", correct: false},
         ]
     },
     { 
         question: "Vilken stad är bäst i Sverige",
         answers: [
-            {text: "Malmö", correct: false},
-            {text: "Göteborg", correct: false},
-            {text: "Stockholm", correct: true},
-            {text: "Borås", correct: false},
+            {text: "Sant", correct: false},
+            {text: "Falskt", correct: false},
         ]
     },
     { 
         question: "Vilken stad är bäst i Sverige",
         answers: [
-            {text: "Malmö", correct: false},
-            {text: "Göteborg", correct: false},
-            {text: "Stockholm", correct: true},
-            {text: "Borås", correct: false},
+            {text: "Sant", correct: false},
+            {text: "Falskt", correct: false},
         ]
     },
     { 
         question: "Vilken stad är bäst i Sverige",
         answers: [
-            {text: "Malmö", correct: false},
-            {text: "Göteborg", correct: false},
-            {text: "Stockholm", correct: true},
-            {text: "Borås", correct: false},
+            {text: "Sant", correct: false},
+            {text: "Falskt", correct: false},
         ]
     },
     { 
         question: "Vilken stad är bäst i Sverige",
         answers: [
-            {text: "Malmö", correct: false},
-            {text: "Göteborg", correct: false},
-            {text: "Stockholm", correct: true},
-            {text: "Borås", correct: false},
+            {text: "Sant", correct: false},
+            {text: "Falskt", correct: false},
         ]
     },
     { 
         question: "Vilken stad är bäst i Sverige",
         answers: [
-            {text: "Malmö", correct: false},
-            {text: "Göteborg", correct: false},
-            {text: "Stockholm", correct: true},
-            {text: "Borås", correct: false},
+            {text: "Sant", correct: false},
+            {text: "Falskt", correct: false},
         ]
     },
     { 
         question: "Vilken stad är bäst i Sverige",
         answers: [
-            {text: "Malmö", correct: false},
-            {text: "Göteborg", correct: false},
-            {text: "Stockholm", correct: true},
-            {text: "Borås", correct: false},
+            {text: "Sant", correct: false},
+            {text: "Falskt", correct: false},
         ]
     },
     { 
         question: "Vilken stad är bäst i Sverige",
         answers: [
-            {text: "Malmö", correct: false},
-            {text: "Göteborg", correct: false},
-            {text: "Stockholm", correct: true},
-            {text: "Borås", correct: false},
+            {text: "Sant", correct: false},
+            {text: "Falskt", correct: false},
         ]
     },
     { 
         question: "Vilken stad är bäst i Sverige",
         answers: [
-            {text: "Malmö", correct: false},
-            {text: "Göteborg", correct: false},
-            {text: "Stockholm", correct: true},
-            {text: "Borås", correct: false},
+            {text: "Sant", correct: false},
+            {text: "Falskt", correct: false},
         ]
     }
 ];
-const frågorElement = document.getElementById("frågor");
-const svarsknapp = document.getElementById("svarsknapp");
-const nästaknapp = document.getElementById("nästaknapp");
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-btn");
+const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -100,6 +80,44 @@ let score = 0;
 function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
-    nästaknapp.innerHTML = "Nästa";
+    nextButton.innerHTML = "Next";
     showQuestion();
 }
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+
+}
+
+startQuiz();
