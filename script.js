22e070f601cc1ad8520eb7f74d0ca2f66a6d998f
Ando-FEND <antonio.brynildsen@hotmail.com>
2024-12-11 00:28:06 +0100

    Fixade väldigt mycket på min stationära, fick inte github att funka där, men allting funkar och nu ska jag fösröka lösa darkmode

--- script.js
+++ script.js
@@ -1,100 +1,96 @@
 const questions = [
-    { 
-        question: "Stockholm är den bästa staden i sverige.",
+    {
+        question: "Jorden är den största planeten i vårt solsystem.",
+        answers: [
+            {text: "Sant", correct: false},
+            {text: "Falskt", correct: true},
+        ]
+    },
+    {
+        question: "Människokroppen består av ungefär 60 % vatten.",
         answers: [
             {text: "Sant", correct: true},
             {text: "Falskt", correct: false},
         ]
     },
-    { 
-        question: "Vilken stad är bäst i Sverige",
+    {
+        question: "Den första människan som gick på månen var Neil Armstrong.",
         answers: [
-            {text: "Sant", correct: false},
+            {text: "Sant", correct: true},
             {text: "Falskt", correct: false},
         ]
     },
-    { 
-        question: "Vilken stad är bäst i Sverige",
+    {
+        question: "Stockholm är Sveriges största stad.",
         answers: [
-            {text: "Sant", correct: false},
+            {text: "Sant", correct: true},
             {text: "Falskt", correct: false},
         ]
     },
-    { 
-        question: "Vilken stad är bäst i Sverige",
+    {
+        question: "Hajen är ett däggdjur.",
         answers: [
             {text: "Sant", correct: false},
-            {text: "Falskt", correct: false},
+            {text: "Falskt", correct: true},
         ]
     },
-    { 
-        question: "Vilken stad är bäst i Sverige",
+    {
+        question: "Kameleonter byter färg för att smälta in i sin omgivning.",
         answers: [
             {text: "Sant", correct: false},
-            {text: "Falskt", correct: false},
+            {text: "Falskt", correct: true},
         ]
     },
-    { 
-        question: "Vilken stad är bäst i Sverige",
+    {
+        question: "Månen har sin egen atmosfär.",
         answers: [
             {text: "Sant", correct: false},
-            {text: "Falskt", correct: false},
+            {text: "Falskt", correct: true},
         ]
     },
-    { 
-        question: "Vilken stad är bäst i Sverige",
+    {
+        question: "Alla fåglar kan flyga.",
         answers: [
             {text: "Sant", correct: false},
-            {text: "Falskt", correct: false},
+            {text: "Falskt", correct: true},
         ]
     },
-    { 
-        question: "Vilken stad är bäst i Sverige",
+    {
+        question: "Vulkaner finns bara på land.",
         answers: [
             {text: "Sant", correct: false},
-            {text: "Falskt", correct: false},
+            {text: "Falskt", correct: true},
         ]
     },
-    { 
-        question: "Vilken stad är bäst i Sverige",
+    {
+        question: "Ljus färdas snabbare än ljud.",
         answers: [
-            {text: "Sant", correct: false},
-            {text: "Falskt", correct: false},
-        ]
-    },
-    { 
-        question: "Vilken stad är bäst i Sverige",
-        answers: [
-            {text: "Sant", correct: false},
+            {text: "Sant", correct: true},
             {text: "Falskt", correct: false},
         ]
     }
 ];
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-btn");
-const nextButton = document.getElementById("next-btn");
-
+const questionElement = document.getElementById("frågor");
+const answerButtons = document.getElementById("svara-knapp");
+const nextButton = document.getElementById("nästa-knapp");
 let currentQuestionIndex = 0;
 let score = 0;
-
 function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
-    nextButton.innerHTML = "Next";
+    nextButton.innerHTML = "Nästa fråga";
+    questionElement.style.color = "black";
     showQuestion();
 }
-
 function showQuestion(){
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.
-    question;
-
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
-        button.classList.add("btn");
+        button.classList.add("knapp");
         answerButtons.appendChild(button);
         if(answer.correct){
             button.dataset.correct = answer.correct;
@@ -108,19 +104,56 @@ function resetState(){
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
-
-function selectAnswer(e){
-    const selectedBtn = e.target;
+function selectAnswer(x){
+    const selectedBtn = x.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++;
     }else{
         selectedBtn.classList.add("incorrect");
     }
-
+    Array.from(answerButtons.children).forEach(button => {
+        button.disabled = true;
+        if (button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = "true";
+    });
+    nextButton.style.display = "block";
 }
-
-
+function showScore(){
+    resetState();
+    let scoreColor;
+    let feedBackMessage;
+    if(score > 7.5 / 10 * questions.length){
+        scoreColor = "green";
+        feedBackMessage = "vad duktig du är!";
+    }else if(score > 4 / 10 * questions.length){
+        scoreColor = "orange";
+        feedBackMessage = "helt ok men du kan bättre";
+    }else if(score < 5 / 10 * questions.length){
+        scoreColor = "red";
+        feedBackMessage = "det är bara att försöka igen polarn...";
+    }
+    questionElement.innerHTML = `Du fick ${score} av ${questions.length} ${feedBackMessage}`;
+    questionElement.style.color = scoreColor;
+    nextButton.innerHTML = "Testa igen!";
+    nextButton.style.display = "block";
+}
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
 startQuiz();
-
-// Knapparna funkar inte något fel i min function eller eventlistener samt går det inte att targeta incorrect/correct i scss
